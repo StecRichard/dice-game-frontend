64d47afc6812ed1d64d9cfeae696196de1091106
Richard Stec <stecrichard5@gmail.com>
2019-09-05 12:01:25 +0200

    Create of dice component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Mezi kozy</h1>
+    <h1>{{numbers}}</h1>
+    <div class="dicesConteiner">
+      <dice v-on:click="selectDice" :key="index" :number="number" v-for="(number, index) in numbers" />
+    </div>
+
+    <div>
+      <button v-on:click="throwDices">Throw Dices</button>
+    </div>
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Dice from "./components/Dice.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
+  data: () => {
+    return {
+      numbers: null,
+      loading: false
+    };
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    Dice
+  },
+  methods: {
+    selectDice() {
+    },
+    throwDices() {
+      if(this.loading){ 
+        return 
+      }
+
+      this.loading = true;
+
+      let intervalId = setInterval(() => {
+        if(this.loading){
+          if(!this.numbers){ 
+            this.numbers = [1,1,1]
+          }
+          
+          this.numbers = this.numbers.map(element => {            
+            element = Math.floor(Math.random() * (7 - 2) + 2); 
+            return element
+          }); 
+        } else {
+          clearInterval(intervalId)
+        }
+      }, 100)
+
+      setTimeout(() => {
+        axios
+          .get("http://localhost:3000/dices")
+          .then(
+            response => {
+              this.numbers = response.data
+              this.loading = false
+            }
+              
+          );
+      }, 3000);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.dicesConteiner {
+  display: flex;
 }
 </style>
---
+++ src/components/Dice.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="diceConteiner">
+    <img :src="renderImg(number)" />
+  </div>  
+</template>
+
+<script>
+import dice1 from "../assets/dice1.png";
+import dice2 from "../assets/dice2.png";
+import dice3 from "../assets/dice3.png";
+import dice4 from "../assets/dice4.png";
+import dice5 from "../assets/dice5.png";
+import dice6 from "../assets/dice6.png";
+
+export default {
+  methods: {
+    renderImg: number => {
+      let diceImgArray = [dice1, dice2, dice3, dice4, dice5, dice6];
+      return diceImgArray[number - 1];
+    }
+  },
+  props: {
+    number: Number
+  }
+};
+</script>
+
+<style scoped>
+img {
+  width: 100px;
+  height: 100px;
+  transition: width 0.5s, height 0.5s;
+}
+
+img:hover {
+  width: 150px;
+  height: 150px;
+  transition: width 0.5s, height 0.5s;
+}
+
+.diceConteiner {
+  height: 150px;
+  width: 150px;
+}
+</style>
